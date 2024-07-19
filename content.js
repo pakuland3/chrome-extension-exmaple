fbff04ba5ef5154db16311f8d331678ca55273d5
pakuland3 <pakuland_1@naver.com>
2024-07-19 12:26:55 +0900

    get date of current cell

--- content.js
+++ content.js
@@ -6,13 +6,20 @@ function reloadAssignments(){
     var rows=tables[0].getElementsByTagName("tr"); // rows for 4-5 weeks of month
     
     const assignmentMap=new Map();
-    
+
     for(var i=rows.length-1;i>0;i--){
         var ul=rows[i].getElementsByTagName("ul"); // ul HTMLCollection for rows
+        var daysOfThisWeek=rows[i].getElementsByClassName("day text-sm-center text-md-left clickable");
         for(var j=ul.length-1;j>-1;j--){
             var a=ul[j].getElementsByTagName("a"); // a HTMLCollection for uls
+            var dateInfo=daysOfThisWeek[j].getElementsByTagName("a")[0];
+            var day=dateInfo.getAttribute("data-day");
+            var month=dateInfo.getAttribute("data-month");
+            var year=dateInfo.getAttribute("data-year");
             for(var k=0;k<a.length;k++){
                 var curString=a[k].title;
+                var href=a[k].href; // get url to crawling 
+                
                 if(assignmentMap.has(curString)){
                     a[k--].remove();
                 }
