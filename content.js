d0da20ecd5110a51d7828ed50e2da27b91d4960d
pakuland3 <pakuland_1@naver.com>
2024-07-23 14:53:44 +0900

    0723 edit

--- content.js
+++ content.js
@@ -21,47 +21,50 @@ chrome.storage.sync.get(['isScriptEnabled'], function (result) {
                     var year=dateInfo.getAttribute("data-year");
                     for(var k=0;k<a.length;k++){
                         var curString=a[k].title;
-                        var href=a[k].href; // get url to crawling 
+                        var href=a[k].getAttribute("href"); // get url to crawling 
                         if(assignmentMap.has(curString)) a[k--].remove();
                         else{
                             assignmentMap.set(curString,1);
-                            $(document).ready(function() {
-                                // 특정 a 태그 선택 (예: 첫 번째 a 태그)
-                                // var url = $('a').first().attr('href');
-                                    if (href) {
-                                        $.ajax({
-                                        url: href,
-                                        method: 'GET',
-                                        success: function(data) {
-                                            // 원하는 변수를 사용하여 HTML 데이터 저장
-                                            var $html=$(data);
-                                            var $span=$html.find(".card card-body").find("span");
-                                            var expireDate=new Array;
-                                            $span.each(function (){
-                                                expireDate.push($(this).text());
-                                                console.log("im here");
-                                            });
-                                            for(var i=0;i<expireDate.length;i++) console.log(expireDate[i]);
-                                            // var endyear=parseInt(expireDate.substr(33,4));
-                                            // var endmonth=parseInt(expireDate.substr(38,2));
-                                            // var endday=parseInt(expireDate.substr(41,2));
-                                            // var endhour=parseInt(expireDate.substr(44,2));
-                                            // var endmin=parseInt(expireDate.substr(47,2));
-                                            // var differ=365*(endyear-year)+30*(endmonth-month)+endday-day;
-                                            // if(differ>0) a[k--].remove();
-                                        },
-                                        error: function(error) {
-                                            console.error('Error fetching HTML:', error);
-                                        }
-                                        });
-                                    } else {
-                                        console.error('No href found for the selected a tag');
-                                    }
-                            });
                         }
                     }
                 }
             }
+            
+            $(document).ready(function() {
+                // 특정 a 태그 선택 (예: 첫 번째 a 태그)
+                // var url = $('a').first().attr('href');
+                    if (href) {
+                        $.ajax({
+                        url: href,
+                        method: 'GET',
+                        success: function(data) {
+                            // 원하는 변수를 사용하여 HTML 데이터 저장
+                            console.log(data);
+                            console.log(href);
+                            // var $html=$(data);
+                            // var $span=$html.find(".card card-body");
+                            // var expireDate=new Array;
+                            // $span.each(function (){
+                            //     expireDate.push($(this).text());
+                            //     console.log("im here");
+                            // });
+                            // for(var i=0;i<expireDate.length;i++) console.log(expireDate[i]);
+                            // var endyear=parseInt(expireDate.substr(33,4));
+                            // var endmonth=parseInt(expireDate.substr(38,2));
+                            // var endday=parseInt(expireDate.substr(41,2));
+                            // var endhour=parseInt(expireDate.substr(44,2));
+                            // var endmin=parseInt(expireDate.substr(47,2));
+                            // var differ=365*(endyear-year)+30*(endmonth-month)+endday-day;
+                            // if(differ>0) a[k--].remove();
+                        },
+                        error: function(error) {
+                            console.error('Error fetching HTML:', error);
+                        }
+                        });
+                    } else {
+                        console.error('No href found for the selected a tag');
+                    }
+            });
         }
         
         reloadAssignments();
