f646847511160858367255268114b21abc6ccb41
pakuland3 <pakuland_1@naver.com>
2024-07-24 12:04:49 +0900

    add perfect time checker

--- content.js
+++ content.js
@@ -1,7 +1,34 @@
 chrome.storage.sync.get(['isScriptEnabled'], function (result) {
     if (result.isScriptEnabled) {
-        // Your content script code here
-        function reloadAssignments(){
+
+        function fetchHTML(href,year,month,day) {
+            return new Promise((resolve, reject) => {
+                $.ajax({
+                    url: href,
+                    method: 'GET',
+                    xhrFields: {
+                        withCredentials: true // 쿠키를 포함하여 요청을 보냄
+                    },
+                    success: function(data) {
+                        // 원하는 변수를 사용하여 HTML 데이터 저장
+                        var $html = $(data);
+                        var expireDate = $html.find(".card:eq(2)").text();
+                        var endyear = parseInt(expireDate.substr(33, 4));
+                        var endmonth = parseInt(expireDate.substr(38, 2));
+                        var endday = parseInt(expireDate.substr(41, 2));
+                        var endhour = parseInt(expireDate.substr(44, 2));
+                        var endmin = parseInt(expireDate.substr(47, 2));
+                        var differ = 365 * (endyear - year) + 30 * (endmonth - month) + (endday - day);
+                        resolve({ endyear, endmonth, endday, differ });
+                    },
+                    error: function(error) {
+                        reject(error);
+                    }
+                });
+            });
+        }
+
+        async function reloadAssignments(){
             const className="calendarmonth calendartable mb-0 table table-coursemos table-bordered";
         
             var tables = document.getElementsByClassName(className);
@@ -9,62 +36,39 @@ chrome.storage.sync.get(['isScriptEnabled'], function (result) {
             var rows=tables[0].getElementsByTagName("tr"); // rows for 4-5 weeks of month
             
             const assignmentMap=new Map();
-        
+
             for(var i=rows.length-1;i>0;i--){
                 var ul=rows[i].getElementsByTagName("ul"); // ul HTMLCollection for rows
                 var daysOfThisWeek=rows[i].getElementsByClassName("day text-sm-center text-md-left clickable");
                 for(var j=ul.length-1;j>-1;j--){
                     var a=ul[j].getElementsByTagName("a"); // a HTMLCollection for uls
                     var dateInfo=daysOfThisWeek[j].getElementsByTagName("a")[0];
-                    var day=dateInfo.getAttribute("data-day");
-                    var month=dateInfo.getAttribute("data-month");
-                    var year=dateInfo.getAttribute("data-year");
+                    var day=parseInt(dateInfo.getAttribute("data-day"));
+                    var month=parseInt(dateInfo.getAttribute("data-month"));
+                    var year=parseInt(dateInfo.getAttribute("data-year"));
+                    // console.log(`${year}/${month}/${day}`);
                     for(var k=0;k<a.length;k++){
                         var curString=a[k].title;
-                        var href=a[k].getAttribute("href"); // get url to crawling 
+                        var href=a[k].getAttribute("href"); // get url to crawling
                         if(assignmentMap.has(curString)) a[k--].remove();
                         else{
                             assignmentMap.set(curString,1);
+                            assignmentMap.set(curString, 1);
+                            if (href) {
+                                try {
+                                    let { endyear, endmonth, endday, differ } = await fetchHTML(href,year,month,day);
+                                    console.log(`${endyear}/${endmonth}/${endday}/${differ}`);
+                                    if (differ>0) a[k--].remove();
+                                } catch (error) {
+                                    console.error('Error fetching HTML:', error);
+                                }
+                            } else {
+                                console.error('No href found for the selected a tag');
+                            }
                         }
                     }
                 }
             }
-            
-            $(document).ready(function() {
-                // 특정 a 태그 선택 (예: 첫 번째 a 태그)
-                // var url = $('a').first().attr('href');
-                    if (href) {
-                        $.ajax({
-                        url: href,
-                        method: 'GET',
-                        success: function(data) {
-                            // 원하는 변수를 사용하여 HTML 데이터 저장
-                            console.log(data);
-                            console.log(href);
-                            // var $html=$(data);
-                            // var $span=$html.find(".card card-body");
-                            // var expireDate=new Array;
-                            // $span.each(function (){
-                            //     expireDate.push($(this).text());
-                            //     console.log("im here");
-                            // });
-                            // for(var i=0;i<expireDate.length;i++) console.log(expireDate[i]);
-                            // var endyear=parseInt(expireDate.substr(33,4));
-                            // var endmonth=parseInt(expireDate.substr(38,2));
-                            // var endday=parseInt(expireDate.substr(41,2));
-                            // var endhour=parseInt(expireDate.substr(44,2));
-                            // var endmin=parseInt(expireDate.substr(47,2));
-                            // var differ=365*(endyear-year)+30*(endmonth-month)+endday-day;
-                            // if(differ>0) a[k--].remove();
-                        },
-                        error: function(error) {
-                            console.error('Error fetching HTML:', error);
-                        }
-                        });
-                    } else {
-                        console.error('No href found for the selected a tag');
-                    }
-            });
         }
         
         reloadAssignments();
